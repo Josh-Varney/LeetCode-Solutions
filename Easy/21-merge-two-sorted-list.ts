c50b90d9336dc5c6c9189f4fd01d3edbd3437383
Joshua Varney <jrv123756@gmail.com>
2026-03-10 16:35:50 +0000

    Update Struct

--- Easy/21-merge-two-sorted-list.ts
+++ Easy/21-merge-two-sorted-list.ts
@@ -1,3 +1,13 @@
+class ListNode {
+    val: number;
+    next: ListNode | null;
+
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
     if (!list1 && !list2){
         return null;
